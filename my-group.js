3f38a9ab9f004fedac8624f0b365bfde6d8ece96
Martin Rosales <j.martin3537@gmail.com>
2025-06-28 16:26:23 +0800

    changes made to grp v1.0

--- my-group.js
+++ my-group.js
@@ -533,3 +533,15 @@ function setupNotificationListener(userId) {
   window.notificationUnsubscribe = unsubscribe;
 }
 
+// Loader
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    if (loader) {
+        loader.classList.add("loader-hidden");
+        loader.addEventListener("transitionend", () => {
+            if (loader.parentNode) {
+                loader.parentNode.removeChild(loader);
+            }
+        });
+    }
+});
